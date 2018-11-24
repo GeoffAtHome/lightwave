5f1baade854f508ee483cb437208a01bfbfa441d
GeoffAtHome <geoff@soord.org.uk>
2018-11-24 18:48:16 +0000

    Refining.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-	name='lightwave',
-	version='0.1',
-	description='Python library to provide a realiable communication link with LightWaveRF lights and switches.',
-	url='https://github.com/GeoffAtHome/lightwave',
-	author='Geoff Soord',
-	author_email='',
-	license='MIT',
-	packages=['lightwave'],
-	zip_safe=False
+    name='lightwave',
+    version='0.12',
+    description='Python library to provide a realiable communication link with LightWaveRF lights and switches.',
+    url='https://github.com/GeoffAtHome/lightwave',
+    author='Geoff Soord',
+    author_email='',
+    license='MIT',
+    packages=['lightwave'],
+    zip_safe=False
 )
