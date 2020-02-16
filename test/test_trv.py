fda5b4c635940782c3274a6cc342622ead5e75a8
ColinRobbins <colin.john.robbins@gmail.com>
2020-02-16 17:19:55 +0000

    fix test

--- test/test_trv.py
+++ test/test_trv.py
@@ -7,6 +7,7 @@ from lightwave import LWLink
 async def main():
     lwLink = LWLink('192.168.10.10')
 
+    lwLink.set_trv_proxy("127.0.0.1", 8787)
     lwLink.set_temperature('R1Dh', 21, "Radiator")
 
 loop = asyncio.get_event_loop()
