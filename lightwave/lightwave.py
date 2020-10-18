d49bb272b6dc25fc0c082d98e360b75a304212a9
ColinRobbins <colin.john.robbins@gmail.com>
2020-10-18 13:56:27 +0100

    Python 3.9 compat

--- lightwave/lightwave.py
+++ lightwave/lightwave.py
@@ -32,7 +32,7 @@ class LWLink():
     def _send_message(self, msg):
         """Add message to queue and start processing the queue."""
         LWLink.the_queue.put_nowait(msg)
-        if LWLink.thread is None or not LWLink.thread.isAlive():
+        if LWLink.thread is None or not LWLink.thread.is_alive():
             LWLink.thread = Thread(target=self._send_queue)
             LWLink.thread.start()
 
